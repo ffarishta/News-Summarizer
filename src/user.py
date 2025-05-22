e37cd87e579fd38f8e8f888158fdb29f9410dd1e
Fatima Farishta <fatimafarishta@fatimas-mbp.home>
2025-05-22 12:59:20 -0400

    removed lines

--- src/user.py
+++
@@ -1,9 +0,0 @@
-class User(object):
-    def __init__(self, *args, username, password):
-        super(User, self).__init__(*args)
-        username = username
-        password = password
-
-d = User("d","d")
-print(d.username)
-        
